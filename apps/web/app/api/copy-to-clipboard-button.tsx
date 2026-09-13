b0ecb907c6cd76081194eaaf0043b70215b0be9a
Dean Kerr <dean.kerr@gmail.com>
2026-09-14 00:30:10 +1000

    web: colocate app/api page code

--- apps/web/app/api/copy-to-clipboard-button.tsx
+++ apps/web/app/api/copy-to-clipboard-button.tsx
@@ -5,7 +5,7 @@ import { CheckIcon, CopyIcon } from 'lucide-react'
 import { useState } from 'react'
 import { toast } from 'sonner'
 
-import { Button } from '../ui/button'
+import { Button } from '@/components/ui/button'
 
 export function CopyToClipboardButton({
   value,
